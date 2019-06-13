2d09f4f5132240bb13558151b5f7cc18195bd71c
Yangmengyuan Zhao <zymy701@gmail.com>
2019-06-13 23:21:24 +0800

    Updates

---
+++ precache-manifest.28c2bc7b75337f59e8d7676f6683b174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27235b89065c0c8ec4707be2ccc9465e",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "f68128fcf7703b0d4b34",
+    "url": "/starwars/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "d331e07acfee975be4a1",
+    "url": "/starwars/static/css/main.52eb91d3.chunk.css"
+  },
+  {
+    "revision": "f68128fcf7703b0d4b34",
+    "url": "/starwars/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "d331e07acfee975be4a1",
+    "url": "/starwars/static/js/main.3df53671.chunk.js"
+  },
+  {
+    "revision": "9cd712b42b70d05ab420",
+    "url": "/starwars/static/js/runtime~main.f4b6364c.js"
+  }
+]);
